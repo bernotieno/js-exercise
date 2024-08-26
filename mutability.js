2158c138b19f4798955fbfb6c9a500aef64d0888
bernaotieno <bernadokumu1@gmail.com>
2024-08-26 13:06:44 +0300

    removed the unnecessary person constant

--- mutability.js
+++ mutability.js
@@ -1,10 +1,3 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-  }
-  
-
 const clone1 = JSON.parse(JSON.stringify(person));
 const clone2 = JSON.parse(JSON.stringify(person));
 const samePerson = person
