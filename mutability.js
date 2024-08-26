395c7a764f7d6d77acd34e9039e7dbfd4949cd9f
bernaotieno <bernadokumu1@gmail.com>
2024-08-26 13:04:53 +0300

    Add deep clones and reference of  object
    - Created  and  as deep copies of the  object using JSON methods.
    - Added  as a reference to the original  object.
    - Updated  object properties: incremented age by 1 and changed country to 'Canada'.

---
+++ mutability.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+  }
+  
+
+const clone1 = JSON.parse(JSON.stringify(person));
+const clone2 = JSON.parse(JSON.stringify(person));
+const samePerson = person
+
+person.age += 1;
+person.country = 'Canada';
