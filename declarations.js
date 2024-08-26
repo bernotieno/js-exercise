9e806b1144366431e79897dc4802878dd8cdca04
bernaotieno <bernadokumu1@gmail.com>
2024-08-26 12:25:23 +0300

    Add constant variables with immutability:
    - : String containing special characters with proper escaping.
    - : Immutable array with values [4, '2'].
    - : Immutable object with primitive values (string, number, boolean, undefined).
    - : Immutable object with nested immutable array and object.

---
+++ declarations.js
@@ -0,0 +1,19 @@
+const escapeStr = '`\\/"\'';
+const arr = Object.freeze([4,2]);
+const obj = Object.freeze({
+
+    str: 'declaration string',
+    num: 43,
+    bool: true,
+    undef: undefined
+
+});
+
+const nested = Object.freeze({
+    arr: [4, undefined, '2'],
+    obj: Object.freeze({
+        str: 'hello',
+        num: 34,
+        bool: false
+    })
+});
