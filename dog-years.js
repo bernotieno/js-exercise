f712ab73e3b95e222443e3824186e4160203568a
bernaotieno <bernadokumu1@gmail.com>
2024-08-26 17:08:58 +0300

    Implement dogYears function to calculate dog's age in dog years on different planets
    - Added  object with orbital periods of planets relative to Earth years.
    - Updated  function to:
      - Convert dog's age from seconds to Earth years.
      - Calculate the age in dog years (assuming 1 dog year equals 7 Earth years).
      - Compute the dog's age on the specified planet based on its orbital period.
      - Return the dog's age in dog years, rounded to two decimal places.

---
+++ dog-years.js
@@ -0,0 +1,24 @@
+const planetYears = {
+
+    earth:   1.0,
+    mercury:   0.2408467,
+    venus:   0.61519726,
+    mars:   1.8808158,
+    jupiter:   11.862615,
+    saturn:   29.447498,
+    uranus:   84.016846,
+    neptune:   164.79132
+
+    };
+
+function dogYears(planet, ageInSecs) {
+    const earthYearsInSec = 31557600;
+
+
+    const ageInEarthYears = ageInSecs / earthYearsInSec;
+    const ageInPlanetYears = ageInEarthYears / planetYears[planet.toLowerCase()];
+    const dogYear = ageInEarthYears * 7
+
+    return parseFloat(dogYear.toFixed(2))
+}
+
