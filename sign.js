21c7281479ea73810a350acb83554f3c2dba6507
bernaotieno <bernadokumu1@gmail.com>
2024-08-27 11:14:58 +0300

    Add sameSign function to check if two numbers have the same sign
    - Implemented sameSign function using if-else statements.
    - The function compares the signs of two numbers and returns true if they have the same sign, false otherwise.

--- sign.js
+++ sign.js
@@ -7,3 +7,11 @@ function sign(num) {
         return 0;
     }
 }
+
+function sameSign(num1, num2) {
+    if (sign(num1) === sign(num2)) {
+        return true;
+    } else {
+        return false;
+    }
+}
