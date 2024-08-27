c8dbe8c9f6957bf52d3a2773674ef82c67c53f85
bernaotieno <bernadokumu1@gmail.com>
2024-08-27 15:04:39 +0300

    Add type-checking utility functions to is object
    - Implemented functions to check value types: number, NaN, string, boolean, undefined, defined, array, object, function, truthy, and falsy.

---
+++ is.js
@@ -0,0 +1,15 @@
+const is = {}
+
+is.num = (str) => typeof str === 'number'
+is.nan = (str) => typeof str === ''
+is.num = (value) => typeof value === 'number' && !isNaN(value);
+is.nan = (value) => typeof value === 'number' && isNaN(value);
+is.str = (value) => typeof value === 'string';
+is.bool = (value) => typeof value === 'boolean';
+is.undef = (value) =>  value === undefined;
+is.def = (value) =>  value !== undefined;
+is.arr = (value) => Array.isArray(value);
+is.obj = (value) => value !== null && typeof value === 'object' && !Array.isArray(value);
+is.fun = (value) => typeof value === 'function';
+is.truthy = (value) => !!value;
+is.falsy = (value) => !value
