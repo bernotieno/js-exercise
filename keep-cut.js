6f8782152c26153e3dce0247932d4303adbcbaa3
bernaotieno <bernadokumu1@gmail.com>
2024-08-29 09:26:13 +0300

    Add string manipulation functions: cutFirst, cutLast, cutFirstLast, keepFirst, keepLast, keepFirstLast

---
+++ keep-cut.js
@@ -0,0 +1,29 @@
+// Function to remove the first 2 characters
+function cutFirst(str) {
+    return str.slice(2);
+}
+
+// Function to remove the last 2 characters
+function cutLast(str) {
+    return str.slice(0, -2);
+}
+
+// Function to remove the first 2 and last 2 characters
+function cutFirstLast(str) {
+    return str.slice(2, -2);
+}
+
+// Function to keep only the first 2 characters
+function keepFirst(str) {
+    return str.slice(0, 2);
+}
+
+// Function to keep only the last 2 characters
+function keepLast(str) {
+    return str.slice(-2);
+}
+
+// Function to keep the first 2 and last 2 characters
+function keepFirstLast(str) {
+    return str.slice(0, 2) + str.slice(-2);
+}
