140aa04917d97806d44006d5736f1f703cbd36e7
bernaotieno <bernadokumu1@gmail.com>
2024-08-29 13:35:56 +0300

    Implemented custom rounding functions (round, ceil, floor, trunc) without using Math, bitwise, or modulo operations.

--- cut-corners.js
+++ cut-corners.js
@@ -1,26 +1,61 @@
 function round(num) {
-    if (num >= 0) {
-        return Math.floor(num + 0.5);
-    } else {
-        return Math.ceil(num - 0.5);
+    const sign = num < 0 ? -1 : 1;
+    num = num * sign; // Work with the absolute value
+
+    // Determine the integer part by continuously subtracting 1
+    let integerPart = 0;
+    while (num >= 1) {
+        num -= 1;
+        integerPart += 1;
     }
+
+    // Check if the remaining fractional part is 0.5 or more
+    if (num >= 0.5) {
+        integerPart += 1;
+    }
+
+    return integerPart * sign;
 }
 
 function ceil(num) {
-    if (num === Math.floor(num)) {
-        return num;
+    const sign = num < 0 ? -1 : 1;
+    num = num * sign; // Work with the absolute value
+
+    let integerPart = 0;
+    while (num > 1) {
+        num -= 1;
+        integerPart += 1;
     }
-    return Math.floor(num) + 1;
+
+    if (num > 0) {
+        integerPart += 1;
+    }
+
+    return integerPart * sign;
 }
 
 function floor(num) {
-    return Math.floor(num);
+    const sign = num < 0 ? -1 : 1;
+    num = num * sign; // Work with the absolute value
+
+    let integerPart = 0;
+    while (num >= 1) {
+        num -= 1;
+        integerPart += 1;
+    }
+
+    return integerPart * sign;
 }
 
 function trunc(num) {
-    if (num >= 0) {
-        return floor(num);
-    } else {
-        return ceil(num);
+    const sign = num < 0 ? -1 : 1;
+    num = num * sign; // Work with the absolute value
+
+    let integerPart = 0;
+    while (num >= 1) {
+        num -= 1;
+        integerPart += 1;
     }
+
+    return integerPart * sign;
 }
