095b7d8ab297f36fee974ad76d7c739eadae83f3
bernaotieno <bernadokumu1@gmail.com>
2024-08-30 20:47:45 +0300

    defined the constant vowel outside of the function

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,9 +1,6 @@
+const vowels = /[aeiouAEIOU]/g;
+
 function vowelDots(str) {
-    const vowels = /[aeiouAEIOU]/g;
-    
     return str.replace(vowels, '$&.');
 }
 
-console.log(vowelDots('hello')); // Output: "he.llo."
-console.log(vowelDots('Vowel Dots')); // Output: "Vo.we.l Do.ts"
-console.log(vowelDots('aeiou')); // Output: "a.e.i.o.u."
