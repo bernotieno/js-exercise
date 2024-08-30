db2ea0fe0d7b5b793dd2df2eaa958c8aa641c1cc
bernaotieno <bernadokumu1@gmail.com>
2024-08-30 21:07:23 +0300

    Fix ionOut function to correctly extract and modify words with ion following t

--- ion-out.js
+++ ion-out.js
@@ -1,6 +1,7 @@
 function ionOut(str) {
     const regex = /\b(t\w*ion)\b/g;
 
-    return (str.match(regex) || [])
-        .map(word => word.replace('ion', ''));
+    const matches = str.match(regex) || [];
+
+    return matches.map(word => word.replace('ion', ''));
 }
