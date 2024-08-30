6090f84991a9c967dd8929127a572ca2b401bbc5
bernaotieno <bernadokumu1@gmail.com>
2024-08-30 16:52:57 +0300

    Add regular expressions for matching hi: normal, begin, end, and beginEnd

---
+++ its-a-match.js
@@ -0,0 +1,11 @@
+// matches any element found 
+const normal = /hi/;
+
+// ^ represent in the begin
+const begin = /^hi/;
+
+// $ represent in the end
+const end = /hi$/;
+
+// ^ $ represent either begining or end
+const beginEnd = /^hi$/;
