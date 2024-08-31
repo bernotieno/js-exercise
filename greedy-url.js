830e748fe4894a3a2a45b3fcfa6123a85b2b946f
bernaotieno <bernadokumu1@gmail.com>
2024-08-31 09:34:48 +0300

    Add functions to extract URLs with varying query parameters: getURL, greedyQuery, and notSoGreedy

---
+++ greedy-url.js
@@ -0,0 +1,17 @@
+function getURL(dataSet) {
+    const urlPattern = /(https?:\/\/[^\s]+)/g;
+    return dataSet.match(urlPattern) || [];
+  }
+  
+  function greedyQuery(dataSet) {
+    const urlPattern = /(https?:\/\/[^\s?]+\?([^&=]+=[^&=]*&){2,}[^&=]+=[^&=]*)/g;
+    return (dataSet.match(urlPattern) || []).filter(url => (url.match(/&/g) || []).length >= 2);
+  }
+  
+  function notSoGreedy(dataSet) {
+    const urlPattern = /(https?:\/\/[^\s?]+\?([^&=]+=[^&=]*&){1,2}[^&=]+=[^&=]*)/g;
+    return (dataSet.match(urlPattern) || []).filter(url => {
+      const queryCount = (url.match(/&/g) || []).length;
+      return queryCount >= 1 && queryCount <= 2;
+    });
+  }
