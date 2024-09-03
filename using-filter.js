b763b61b9c727f74cd117926f73af65dc29f3c58
bernaotieno <bernadokumu1@gmail.com>
2024-09-03 15:50:23 +0300

    fix the entire functions to and broken down with simpler syntax

--- using-filter.js
+++ using-filter.js
@@ -1,28 +1,27 @@
-function filterShortStateName(arr) {
-    return arr.filter(state => state.length < 7);
-  }
+const filterShortStateName = (arr) => arr.filter(str => str.length < 7);
+const filterStartVowel = (arr) => arr.filter(str => /^[aeiou]/i.test(str));
 
-  function filterStartVowel(arr) {
-    return arr.filter(str => /^[aeiou]/i.test(str));
-  }
+const filter5Vowels = (arr) => {
+    const vowelCount = (str) => (str.match(/[aeiou]/gi) || []).length;
+    return arr.filter(str => vowelCount(str) >= 5);
+};
 
-  function filter5Vowels(arr) {
-    return arr.filter(str => (str.match(/[aeiou]/gi) || []).length >= 5);
-  }
+const filter1DistinctVowel = (arr) => {
+    const distVowel = (str) => {
+        const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+        const distinctOne = new Set(str.toLowerCase().split('').filter(char => vowels.has(char)));
+        return distinctOne.size;
+    };
+    return arr.filter(str => distVowel(str) === 1);
+};
 
-  function filter1DistinctVowel(arr) {
-    return arr.filter(str => {
-      const vowels = str.match(/[aeiou]/gi) || [];
-      return new Set(vowels).size === 1;
+const multiFilter = (arr) => {
+    return arr.filter(obj => {
+        return(
+            obj.capital.length >= 8 &&
+            !/^[aeiou]/i.test(obj.name) &&
+            /[aeiou]/i.test(obj.tag) &&
+            obj.region !== 'South'
+        );
     });
-  }
-
-  function multiFilter(arr) {
-    return arr.filter(obj => 
-      obj.capital.length >= 8 &&
-      !/^[aeiou]/i.test(obj.name) &&
-      /[aeiou]/i.test(obj.tag) &&
-      obj.region !== "South"
-    );
-  }
-  
+};
