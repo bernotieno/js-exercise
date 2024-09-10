174ebb57f64c335a11d9a4f52af4ae13b21b991c
bernaotieno <bernadokumu1@gmail.com>
2024-09-10 13:45:54 +0300

    add functions that manupilates objects

---
+++ manipulate-values.js
@@ -0,0 +1,26 @@
+const filterValues = (obj, callback) => {
+    return Object.fromEntries(
+      Object.entries(obj).filter(([key, value]) => callback(value, key, obj))
+    );
+  };
+  
+  const mapValues = (obj, callback) => {
+    return Object.fromEntries(
+      Object.entries(obj).map(([key, value]) => [key, callback(value, key, obj)])
+    );
+  };
+  
+  const reduceValues = (obj, callback, initialValue) => {
+    return Object.values(obj).reduce(callback, initialValue);
+  };
+
+  // const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
+
+  // console.log(filterValues(nutrients, (nutrient) => nutrient <= 12))
+  // // output: { carbohydrates: 12, fat: 5 }
+  
+  // console.log(mapValues(nutrients, (v) => v+1))
+  // // output: { carbohydrates: 13, protein: 21, fat: 6 }
+  
+  // console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
+  // // output: 37
