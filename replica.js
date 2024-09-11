66d961eb5dae4d5219725608d6f89b5eee12ed61
bernaotieno <bernadokumu1@gmail.com>
2024-09-11 14:58:51 +0300

    included the is regexp in the function

--- replica.js
+++ replica.js
@@ -1,8 +1,12 @@
-function replica(targetobj, ...sources) {
+function replica(target, ...sources) {
     const isObject = (item) => item && typeof item === 'object' && !Array.isArray(item);
   
+    const isRegExp = (item) => item instanceof RegExp;
+  
     const deepCopy = (value) => {
-      if (isObject(value)) {
+      if (isRegExp(value)) {
+        return new RegExp(value.source, value.flags);
+      } else if (isObject(value)) {
         return Object.keys(value).reduce((acc, key) => {
           acc[key] = deepCopy(value[key]);
           return acc;
@@ -14,25 +18,27 @@ function replica(targetobj, ...sources) {
       }
     };
   
-    if (!isObject(targetobj)) {
-      targetobj = {};
+    if (!isObject(target)) {
+      target = {};
     }
   
     for (const source of sources) {
       if (isObject(source)) {
         for (const key in source) {
           if (Object.prototype.hasOwnProperty.call(source, key)) {
-            if (isObject(source[key])) {
-              targetobj[key] = replica(isObject(targetobj[key]) ? targetobj[key] : {}, deepCopy(source[key]));
+            if (isRegExp(source[key])) {
+              target[key] = new RegExp(source[key].source, source[key].flags);
+            } else if (isObject(source[key])) {
+              target[key] = replica(isObject(target[key]) ? target[key] : {}, deepCopy(source[key]));
             } else if (Array.isArray(source[key])) {
-              targetobj[key] = deepCopy(source[key]);
+              target[key] = deepCopy(source[key]);
             } else {
-              targetobj[key] = source[key];
+              target[key] = source[key];
             }
           }
         }
       }
     }
   
-    return targetobj;
+    return target;
   }
