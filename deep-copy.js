cc98096392fe2fb870785481731833d27da5f87b
bernaotieno <bernadokumu1@gmail.com>
2024-09-11 14:07:19 +0300

    added Date and Regexp to the object

--- deep-copy.js
+++ deep-copy.js
@@ -1,18 +1,30 @@
 function deepCopy(obj) {
-    if (obj === null || typeof obj !== 'object') {
-      return obj;
-    }
-  
-    if (Array.isArray(obj)) {
-      return obj.map(item => deepCopy(item));
-    }
-  
-    const newObj = {};
-    for (const key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        newObj[key] = deepCopy(obj[key]);
-      }
-    }
-  
-    return newObj;
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
   }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => deepCopy(item));
+  }
+
+  if (obj instanceof Date) {
+    return new Date(obj.getTime());
+  }
+
+  if (obj instanceof RegExp) {
+    return new RegExp(obj);
+  }
+
+  if (typeof obj === 'function') {
+    return obj;
+  }
+
+  const newObj = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      newObj[key] = deepCopy(obj[key]);
+    }
+  }
+
+  return newObj;
+}
