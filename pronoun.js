c90a57a7095662a46239a8e26ca0fbc1ad4b7042
bernaotieno <bernadokumu1@gmail.com>
2024-09-11 15:21:07 +0300

    add function that returns pronouns present in a string through an object

---
+++ pronoun.js
@@ -0,0 +1,24 @@
+function pronoun(str) {
+    const pronouns = ['i', 'you', 'he', 'she', 'it', 'they', 'we'];
+    const result = {};
+  
+    const lowercaseStr = str.toLowerCase();
+  
+    const words = lowercaseStr.split(/\s+/);
+  
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (pronouns.includes(word)) {
+        if (!result[word]) {
+          result[word] = { word: [], count: 0 };
+        }
+        result[word].count++;
+        
+        if (i < words.length - 1 && !pronouns.includes(words[i + 1])) {
+          result[word].word.push(words[i + 1]);
+        }
+      }
+    }
+  
+    return result;
+  }
