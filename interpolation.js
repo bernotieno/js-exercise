f11fe0fb8073472bb42b87c209d9e1c5a8c5c15b
bernaotieno <bernadokumu1@gmail.com>
2024-09-13 21:48:26 +0300

    declared the i with let

--- interpolation.js
+++ interpolation.js
@@ -2,7 +2,7 @@ function interpolation({ step, start, end, callback, duration }) {
   const stepSize = (end - start) / step;
   const timeStep = duration / step;
 
-  for (i=1; i<=step;i++){
+  for (let i=1; i<=step;i++){
     const distance = Number((start + (i-1)*stepSize).toFixed(2));
     const point = Number((timeStep * i).toFixed(2));
 
