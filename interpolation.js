df07d7377caab0f0b4306ea4074d6a82cf283046
bernaotieno <bernadokumu1@gmail.com>
2024-09-13 19:51:29 +0300

    return the run step to start with 1

--- interpolation.js
+++ interpolation.js
@@ -1,9 +1,9 @@
 function interpolation({ step, start, end, callback, duration }) {
-    const stepSize = (end - start) / (step - 1);
-    const timeStep = duration / (step - 1);
+    const stepSize = (end - start) / step;
+    const timeStep = duration / step;
   
     function runStep(i) {
-      if (i >= step) return;
+      if (i > step) return;
   
       const distance = start + stepSize * i;
       const point = timeStep * i;
@@ -16,6 +16,6 @@ function interpolation({ step, start, end, callback, duration }) {
       }, point);
     }
   
-    // Start from 0 instead of 1
-    runStep(0);
+    runStep(1);
   }
+  
