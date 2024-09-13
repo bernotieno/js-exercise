dfcb3fa692c75e2ea5318ba97956719cd5789c30
bernaotieno <bernadokumu1@gmail.com>
2024-09-13 18:56:41 +0300

    modify the implementation to address the issue

--- interpolation.js
+++ interpolation.js
@@ -2,16 +2,12 @@ function interpolation({ step, start, end, callback, duration }) {
     const stepSize = (end - start) / step;
     const delayBetweenSteps = duration / step;
   
-    function executeStep(currentStep) {
-      if (currentStep >= step) return;
-  
-      const distance = start + stepSize * currentStep;
-      const point = delayBetweenSteps * currentStep;
-  
-      callback([distance, point]);
-  
-      setTimeout(() => executeStep(currentStep + 1), delayBetweenSteps);
+    for (let i = 0; i < step; i++) {
+      const distance = start + stepSize * i;
+      const point = delayBetweenSteps * i;
+      
+      setTimeout(() => {
+        callback([distance, point]);
+      }, point);
     }
-  
-    executeStep(0);
   }
