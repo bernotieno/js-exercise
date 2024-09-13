4d854dbe2fdb9ee2d5ec33b8715d246c56eb56db
bernaotieno <bernadokumu1@gmail.com>
2024-09-13 19:09:57 +0300

    fix the function to work properly

--- interpolation.js
+++ interpolation.js
@@ -1,13 +1,18 @@
 function interpolation({ step, start, end, callback, duration }) {
     const stepSize = (end - start) / step;
-    const delayBetweenSteps = duration / step;
+    const timeStep = duration / step;
+  
+    // console.log(`Interpolation called with step: ${step}, start: ${start}, end: ${end}, duration: ${duration}`);
   
     for (let i = 0; i < step; i++) {
       const distance = start + stepSize * i;
-      const point = delayBetweenSteps * i;
+      const point = timeStep * i;
       
       setTimeout(() => {
+        // console.log(`Executing callback at ${Date.now()}: [${distance}, ${point}]`);
         callback([distance, point]);
       }, point);
     }
+  
+    // console.log('All callbacks scheduled');
   }
