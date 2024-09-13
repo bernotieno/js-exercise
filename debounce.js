4402896ed3b16689c4e1ab5ec06eaac7cb970a7d
bernaotieno <bernadokumu1@gmail.com>
2024-09-13 14:57:07 +0300

    fix the opdebounce function to include the date now

--- debounce.js
+++ debounce.js
@@ -16,24 +16,26 @@ function debounce(func, wait) {
   // Debounce function with leading option
   function opDebounce(func, wait, options = {}) {
     let timeout;
-    let isLeadingCall = true;
+    let lastCallTime = 0;
     
     return function executedFunction(...args) {
+      const currentTime = Date.now();
+      const isInvoking = options.leading && (currentTime - lastCallTime > wait);
+  
       const later = () => {
         timeout = null;
         if (!options.leading) {
+          lastCallTime = Date.now();
           func(...args);
         }
       };
   
-      const callNow = options.leading && isLeadingCall;
-      
+      if (isInvoking) {
+        lastCallTime = currentTime;
+        return func(...args);
+      }
+  
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
-      
-      if (callNow) {
-        isLeadingCall = false;
-        func(...args);
-      }
     };
   }
