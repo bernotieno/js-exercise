caa393ef5bc191814778033d104906bd13e75fd1
bernaotieno <bernadokumu1@gmail.com>
2024-09-14 03:56:29 +0300

    Add functions to race promises and resolve when a specified number of promises complete

---
+++ race.js
@@ -0,0 +1,46 @@
+function race(promises) {
+    return new Promise((resolve, reject) => {
+      if (promises.length === 0) {
+        return;
+      }
+      promises.forEach(promise => {
+        Promise.resolve(promise).then(resolve, reject);
+      });
+    });
+  }
+  
+  function some(promises, count) {
+    return new Promise((resolve) => {
+      if (promises.length === 0 || count === 0) {
+        resolve(undefined);
+        return;
+      }
+  
+      const results = [];
+      let resolved = 0;
+      let completed = 0;
+  
+      promises.forEach((promise, index) => {
+        Promise.resolve(promise)
+          .then(value => {
+            if (resolved < count) {
+              results[index] = value;
+              resolved++;
+              if (resolved === count) {
+                resolve(results.filter(result => result !== undefined));
+              }
+            }
+            completed++;
+            if (completed === promises.length && resolved < count) {
+              resolve(results.filter(result => result !== undefined));
+            }
+          })
+          .catch(() => {
+            completed++;
+            if (completed === promises.length && resolved < count) {
+              resolve(results.filter(result => result !== undefined));
+            }
+          });
+      });
+    });
+  }
