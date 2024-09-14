e3e587579c448eb6f4de063a406dab99dca62972
bernaotieno <bernadokumu1@gmail.com>
2024-09-14 03:35:22 +0300

    fix to work with synchronous values

--- all.js
+++ all.js
@@ -2,15 +2,16 @@ function all(obj) {
     // Get the entries (key-value pairs) of the object
     const entries = Object.entries(obj);
   
-    // If there are no entries, return an empty object immediately
+    // Return an empty object immediately if the input object is empty
     if (entries.length === 0) return Promise.resolve({});
   
-    // Create an array of promises, each resolving to [key, value]
+    // Create an array of promises, each resolving to [key, resolvedValue]
     const promises = entries.map(([key, value]) => 
+      // Ensure all values, whether synchronous or promise, are wrapped in Promise.resolve
       Promise.resolve(value).then(resolvedValue => [key, resolvedValue])
     );
   
-    // Use the built-in Promise.all on our array of promises
+    // Use Promise.all to resolve all promises in the array
     return Promise.all(promises).then(resolvedEntries => 
       // Convert the resolved entries back into an object
       Object.fromEntries(resolvedEntries)
