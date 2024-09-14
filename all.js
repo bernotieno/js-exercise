c00ce72799148b68b3c74cd9d4ab9b5e27891160
bernaotieno <bernadokumu1@gmail.com>
2024-09-14 03:39:50 +0300

    fix for the synchronous values and wait for all promises to resolve for asynchronous values

--- all.js
+++ all.js
@@ -1,20 +1,24 @@
 function all(obj) {
-    // Get the entries (key-value pairs) of the object
-    const entries = Object.entries(obj);
+    return new Promise((resolve, reject) => {
+      const keys = Object.keys(obj);
+      const result = {};
+      let completed = 0;
   
-    // Return an empty object immediately if the input object is empty
-    if (entries.length === 0) return Promise.resolve({});
+      if (keys.length === 0) {
+        resolve(result);
+        return;
+      }
   
-    // Create an array of promises, each resolving to [key, resolvedValue]
-    const promises = entries.map(([key, value]) => 
-      // Ensure all values, whether synchronous or promise, are wrapped in Promise.resolve
-      Promise.resolve(value).then(resolvedValue => [key, resolvedValue])
-    );
-  
-    // Use Promise.all to resolve all promises in the array
-    return Promise.all(promises).then(resolvedEntries => 
-      // Convert the resolved entries back into an object
-      Object.fromEntries(resolvedEntries)
-    );
+      keys.forEach(key => {
+        Promise.resolve(obj[key])
+          .then(value => {
+            result[key] = value;
+            completed++;
+            if (completed === keys.length) {
+              resolve(result);
+            }
+          })
+          .catch(reject);
+      });
+    });
   }
-  
