cac3763c2ff5477575dfe756c8204bb5e61a5379
bernaotieno <bernadokumu1@gmail.com>
2024-09-14 03:48:46 +0300

    ensures that all functions are executed in series until an error occurs

--- series.js
+++ series.js
@@ -2,14 +2,10 @@ async function series(asyncFunctions) {
     const results = [];
   
     for (const asyncFunc of asyncFunctions) {
-      try {
-        // Execute each function and await its result
-        const result = await asyncFunc();
-        results.push(result);
-      } catch (error) {
-        // If any function throws an error, add it to the results array
-        results.push(error);
-      }
+      // Execute each function and await its result
+      // If any function throws, the error will propagate
+      const result = await asyncFunc();
+      results.push(result);
     }
   
     return results;
